309936a727eaba0ffc37934b679682daa5755639
ichi_taro3 <taro3.ichi@gmail.com>
2020-03-24 23:35:39 +0900

    Initial Commit

---
+++ jikokuhyo-timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jikokuhyo-timer
+//
+//  Created by Kotaro Ichihara on 2020/03/24.
+//  Copyright © 2020 Kotaro Ichihara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
